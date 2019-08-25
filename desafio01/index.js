af5f7864ef465a4f2925cab40ac2779b63cd83aa
IZaldoBacelar <izaldo.figueiroa.b@gmail.com>
2019-08-25 13:55:45 -0300

    Finalizando o desafio 01 do Bootcamp 8.0

--- desafio01/index.js
+++ desafio01/index.js
@@ -2,37 +2,71 @@ const express = require('express');
 const server = express();
 
 server.use(express.json());
-
+let request = 0;
 const projects = [];
 
-server.get('/projects', (req, res) =>{
-  return res.json(projects);
-});
+server.use((req,res,next)=>{
+  request++;
 
-server.post('/projects', (req, res)=>{  //Rota recebendo um campo title e aramzenando uma nova trefa no array ...    
-  const projeto = {id,title,task} = req.body;
+  console.log(`O numero de requisicoes foi de: ${request}`)
+
+  return next();
+})
+
+function checarProject(req, res, next){
+  const { id } = req.params;
+
+  if(!projects[id]){
+    return res.status(400).json({error: "Nao existe projeto meu filho"});
+  }
+  return next();
   
-  projects.push(projeto)
+}
+
+server.get('/projects',checarProject, (req, res) =>{ //listar projetos  
+  return res.json(projects);
+  
+});
+//ok
+server.post('/projects', (req, res)=>{  //criando o projeto com 3 itens: id, title,task
+  const projeto = {id,title,tasks} = req.body;
+  
+  projects.push(projeto);
   
   return res.json(projects);
   });
 
-server.put('/projects/:id', (req, res) =>{
+  //ok
+server.put('/projects/:id', checarProject,(req, res) =>{ // Aterar titulo do projeto
   const { id } = req.params;
   const { title } = req.body;
   
   projects.title = title;
-  projects [id].title =title
+  projects [id].title =title;
+  
   return res.json(projects);
 
 });  
+
+//ok
+server.delete('/projects/:id',checarProject, (req,res)=>{
+  const { id } = req.params;
   
-
-  server.post('/projects/:id/tasks', (req, res ) =>{
-
+  projects.splice(id, 1);
 
   return res.json(projects);
-});
+
+})
+//ok
+server.post('/projects/:id/tasks',checarProject, (req,res) =>{
+  const { title } = req.body;
+  const { id } = req.params;
+
+  projects[id].tasks.push(title);
+
+  return res.json(projects);
+})
+  
 
 
 server.listen(3333);
